94e15e564fa2f75f2651e17a4b7f7a099d1347ce
RafaelFrire <rafaelfesousa@hotmail.com>
2023-11-29 10:44:23 -0300

    created methdos

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const { Client } = require("pg")
 
-
+// método connect gera conexão com o banco de dados
 async function connect(){
 
 
@@ -32,13 +32,86 @@ async function connect(){
     
 }
 connect();
-
+// Método para buscar todos os usuários do BD
 async function selectUsers(){
     const client = await connect(); 
     const res = await client.query("SELECT * FROM tbl_usuarios");
     return res.rows
 }
+// metodo para buscar um usuário com ID específico
+async function selectUser(id){
+    const client = await connect()
+    const response = await client.query("select * from tbl_usuarios where id_usuario=$1", [id])
+    return response.rows
+}
+// método para cadastrar usuários:
+async function registerUser(user){
+    const client = await connect();
+    const values =  [user.email, user.senha]
+    const sql = "INSERT INTO tbl_usuarios(email, senha) VALUES ($1, $2)";
+    await client.query(sql, values);
+}
+
+async function deleteUser(id){
+    const client = await connect();
+    try{
+        const sql = "DELETE FROM tbl_usuarios WHERE id_usuario=$1";
+        const values = [id];
+        await client.query(sql, values)
+    }
+    catch (error){
+        console.error("Usuário não encontrado: ")
+    }
+    finally{
+        client.release();
+    }
+
+    
+}
+
+
+// métodos para a tabela cursos do BD
+
+async function getCourse(id){
+    const client = await connect();
+    const sql = "SELECT * FROM tbl_cursos WHERE id=$1"
+    const response = await client.query(sql, [id]);
+    return response.rows
+}
+
+
+async function getCourses(){
+    const client = await connect();
+    const sql = "SELECT * FROM tbl_cursos";
+    const response = await client.query(sql)
+    return response.rows
+
+}
+
+
+async function registerCourse(data){
+    const client = await connect();
+    const sql = "INSERT INTO tbl_cursos (nome_curso, nome_professor, categoria, descricao) VALUES ($1, $2, $3, $4)" 
+    values = [data.nome_curso, data.nome_professor, data.categoria, data.descricao];
+    await client.query(sql, values);
+
+ }
+
+ async function deleteCourse(id){
+    const client = await connect();
+    const sql = "DELETE FROM tbl_cursos WHERE id=$1";
+    await client.query(sql, [id]);
+ }
+
+
 
 module.exports = {
-    selectUsers
+    selectUsers,
+    selectUser,
+    registerUser,
+    deleteUser,
+    getCourses,
+    getCourse,
+    registerCourse,
+    deleteCourse
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
-
 const db = require("./db")
 
 const port = 3000;
+
 const express = require("express");
-
-
-
 const app = express();
+
+app.use(express.json());
+// recebendo requisições do tipo JSON
+
+
+
+
 
 app.get('/', (req, res) =>{
     return res.json({
@@ -16,12 +20,70 @@ app.get('/', (req, res) =>{
     });
 })
 
+
+// end point para usuário específico
+app.get('/users/:id', async (req, res)=>{
+    const user = await db.selectUser(req.params.id);
+    return res.json(user);
+})
+
+// end point que busca todos os usuários
 app.get('/users', async (req, res)=>{
     const users = await db.selectUsers();
     return res.json(users);
 })
 
-app.listen(port, ()=>{
-    console.log("ESCUTANDO");
-});
+app.post('/users', async (req, res)=>{
+    try{
+        await db.registerUser(req.body);
+        res.sendStatus(201);
+    }catch (error){
+        console.error("erro usuário já registrado: ", error);
+        res.status(500).send("Internal Server Error")
+    }
+   
+})
+
+app.delete('/users/:id', async(req, res)=>{
+    await db.deleteUser(req.params.id);
+    res.status(204);
+
+})
+
+
+// ROTA CURSOS
+
+app.get('/courses/:id', async (req, res)=>{
+    const course = await db.getCourse(req.params.id)
+    return res.json(course);
+})
+
+app.get('/courses', async (req, res)=>{
+    const courses = await db.getCourses();
+    return res.json(courses);
+})
+
+
+
+app.post('/courses', async (req, res)=>{
+    
+    try{
+        db.registerCourse(req.body);
+        res.sendStatus(201);
+    }
+    catch{
+        console.error("error curso já cadastrado registrado: ", error);
+        res.status(500).send("Internal Server Error")
+    }
+})
+
+
+app.delete('/courses/:id', async (req, res)=>{
+    db.deleteCourse(req.params.id);
+    return res.status(204);
+})
+
+
+
+app.listen(port);
 
